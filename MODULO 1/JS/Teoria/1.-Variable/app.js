0577364ae117fb4496b7b337580db4739b1df711
pedroleni <pedroleridanieto@hotmail.com>
2024-03-19 13:17:04 +0100

    TEORIA 19

---
+++ MODULO 1/JS/Teoria/1.-Variable/app.js
@@ -0,0 +1,72 @@
+//! ---------> NAMING
+
+/// ----------------> No podemos meter solo numeros:  let 2 = "Hola"
+/// ----------------> Utilizar camelCase ------->  let holaMundo
+/// ----------------> Evitar las palabras reservadas ---> let, break, if .....
+/// ----------------> No ponemos guiones en medio ---> let nombre-apellido /// correcto _-> let nombreApellido
+/// ----------------> No utilizar tildes ni ñ
+
+//? --------------------> CONCEPTOS BASICOS DE LAS VARIABLES--------
+
+//? -----------> 1) DECLARAR UNA VARIABLE ----->let name ( tipo de dato )
+//? -----------> 2) INICIALIZAR UNA VARIABLE ----> let name = " Pedro "
+//? -----------> 3) NO HACE FALTA INICIALIZAR UNA VARIABLE --->  let name (valorr no definido undefined)
+
+// declaracion
+
+let example;
+
+// incializacion de la variable
+
+example = "prueba";
+
+// declaracion y una inicializacion
+let name = "pedro";
+
+// declaraciones multiples
+
+let nombre = "-",
+  apellido = "pedro";
+
+nombre, (apellido = 34);
+
+console.log(apellido);
+
+///! tipos let const var
+
+const pi = 3.14;
+// ----> let name , como ya esta declarada no permite su redeclaracion
+var miGato = "rodolfo";
+var miGato = 34; // permite la redeclaracion
+
+//pi = "hola que tal "; ---> como es constante no puedo redeclararla ni darle un nuevo valor
+
+// ---------_> SCOPE ---------
+
+const funcionFlecha = () => {
+  let name = "Antonio";
+  console.log("name", name);
+  //pi = "LJKDSFJGHFG"; ---> da error porque es una constante de un primitivo (number, undefined, null, boolean, string) que no se puede cambiar
+};
+
+funcionFlecha();
+console.log("name fuera", name);
+
+/// EJEMPLO CON UN BUCLE
+
+let exampleArray = [1, 2, 3, 4]; /// tipo de dato complejo que tiene posiciones
+
+for (let i = 0; i < exampleArray.length; i++) {
+  name = i;
+  console.log("el valor de la i", name);
+  console.log(`en la posicon del array ${i} ----> ${exampleArray[i]} `); //exampleArray[i] ---> para sacar el valor de una posicion es con el del array y entre corchete el numero de la posicion del array
+}
+
+console.log(name); // el valor de name cambia a 3 porque dentro del bucle no puse let name
+
+//! ----> BLOQUES ---> FUNCTION, BUCLES,  CONDICIONALES (IF ELSE SWITCH ...) lo que lleva una llave suele ser un bloque
+
+const sumar = (one, two) => one + two; // esta devolviendo el resultado sin poner un return IMPLICITO PORQUE ESTA EN UNA LINEA
+const restar = (one, two) => {
+  return one - two; // return explicito , cuando tenga llaves hace falta
+};
---
+++ MODULO 1/JS/Teoria/2.-Tipo de dato/app.js
@@ -0,0 +1,200 @@
+//! ------------------------------------------------------------------------------------
+//? -----------------------------------DATOS PRIMITIVOS---------------------------------
+//! ------------------------------------------------------------------------------------
+
+//todo -------------------------- string
+
+let name = "Pedro";
+name = "Pedro";
+name = `Pedro`;
+
+// ----> concatenar
+let nombreCompleto = name + " " + "lerida";
+nombreCompleto = `${name} lerida`;
+console.log(nombreCompleto);
+
+const products = ["Asus Rog Ally", "Macbook", "fregadora"];
+
+for (let i = 0; i < products.length; i++) {
+  const cartaComponenteDeProductos = `
+    <figure>
+        <h3>${products[i]}</h3>
+    </figure>
+`;
+
+  console.log(cartaComponenteDeProductos);
+}
+
+//todo --------------------------- number
+
+let age = 30;
+
+age += 50; // lo que
+age++;
+age--;
+age /= 2;
+
+let resto = age % 2; // resto de operacion
+console.log(resto);
+
+// -----> concatenar number con un  string ---> PRIORIZA UN STRING
+
+let operacion = 25 + 5 + "hola";
+console.log(operacion); // saca un string 30hola
+operacion = "hola" + 25 + 5;
+console.log(operacion); // saca un string hola255
+operacion = `hola ${25 + 5}`; /// -----> con el template string
+console.log(operacion); // saca un string
+const pi = 3.14; // --> decimal
+const price = 3_000_000; //
+console.log(price);
+
+/**
+ * = ---> es asignacion de valor
+ * == ----> comparacion de valor
+ * === ---> comparacion de valor y de tipo
+ */
+if (30 === "30") {
+  console.log("esto es igual");
+} else {
+  console.log("no son iguales");
+}
+
+//todo-------------------------------------  boolean
+let verdadero = true; // 1
+let falso = false; // 0
+
+//todo ------------------------------------ undefined
+let nombre; // no tiene valor definido por lo cual es undefined- no se ha inicializado
+
+//  null
+let nada = null; // ES NADA Y ESTA DEFINIDO
+
+//! ------------------------------------------------------------------------------------
+//? -----------------------------------DATOS COMPLEJOS----------------------------------
+//! ------------------------------------------------------------------------------------
+
+//todo------------------------------------ object - object {}
+
+/**
+ * Son un conjunto de datos compuesto por claves y valores
+ * tienen llaves {}
+ * las claves son name, age, adress
+ * los valor sera de name = "Pedro"
+ */
+
+const profesor = {
+  name: "Pedro",
+  age: 30,
+  adress: {
+    cp: 28029,
+    city: "Madrid",
+  },
+};
+
+const profesorTwo = {
+  name: "Pedro",
+  age: 30,
+  adress: {
+    cp: 28029,
+    city: "Madrid",
+  },
+};
+
+/**
+ * DOS FORMAS DE ACCEDER A LA POSICION DE LA CLAVE
+ * --> CON EL PUNTO Y EL NOMBRE DE LA CLAVE profesor.name
+ * --> NOMBRE DEL OBJETO Y EL CORCHETE CON EL NOMBRE DE LA CLAVE profesorTwo["name"]
+ */
+if (profesor.name === profesorTwo["name"]) {
+  console.log("son iguales los profesores");
+} else {
+  console.log("no son iguales los profesores");
+}
+
+/**
+ * LOS METODOS LLEVAN PARENTESIS AL FINAL
+ * toString()
+ *
+ * LAS PROPIEDADES NO LLEVAN PARENTESIS
+ * .length
+ */
+
+if (profesor.toString() === profesorTwo.toString()) {
+  console.log("son iguales los profesores ----");
+} else {
+  console.log("no son iguales los profesores-----");
+}
+
+let letra = "A";
+let letraTwo = "A";
+if (letra === letraTwo) {
+  console.log("son iguales ");
+} else {
+  console.log("no son iguales");
+}
+
+// const en los object object
+
+profesor.name = "laura";
+profesor["curso"] = "PT";
+delete profesor.adress;
+// profesor = {}; ----> no te deja porque estas incializandolo de nuevo la constante
+console.log(profesor);
+
+/**
+ * en el caso de los object object puedo cambiar sus clave y valores aunque sea const
+ * pero no puedo cambiar su tipo object object
+ */
+
+//todo ----------------------------------- object - array []
+
+const exampleArray = [
+  {
+    name: "Pedro",
+    age: 30,
+    adress: {
+      cp: 28029,
+      city: "Madrid",
+    },
+  },
+  {
+    name: "Pedro",
+    age: 30,
+    adress: {
+      cp: 28029,
+      city: "Madrid",
+    },
+  },
+  {
+    name: "Pedro",
+    age: 45,
+    adress: {
+      cp: 28029,
+      city: "Madrid",
+    },
+  },
+  {
+    name: "lucio",
+    age: 30,
+    adress: {
+      cp: 28029,
+      city: "Madrid",
+    },
+  },
+];
+
+delete exampleArray[3].age;
+console.log(exampleArray[3]);
+
+/** EJERCICIO-----
+ * vamos a cambiarles a los que tenga 30 años el codigo postal a 28000
+ */
+
+for (let i = 0; i < exampleArray.length; i++) {
+  if (exampleArray[i].age === 30) {
+    exampleArray[i].adress.cp = 28000;
+  }
+}
+
+console.log(exampleArray);
