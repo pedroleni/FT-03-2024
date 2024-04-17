ccfe4aa7cf8dd20d1b89db522ca42b938255dabb
pedroleni <pedroleridanieto@hotmail.com>
2024-04-17 11:19:17 +0200

    check new user

---
+++ MODULO 2/Creacion de API rest/4.-Controladores y rutas/4.4. Login/src/api/routes/User.routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const UserRoutes = express.Router();
+
+const {
+  registerLargo,
+  registerUtil,
+  registerWithRedirect,
+  sendMailRedirect,
+  resendCode,
+  checkNewUser,
+} = require("../controllers/User.controllers");
+const { upload } = require("../../middleware/files.middleware");
+
+//!------------------------------------------------------------------------
+//?--------------------------------RUTAS SIN REDIRECT----------------------
+//!------------------------------------------------------------------------
+
+UserRoutes.post("/registerLargo", upload.single("image"), registerLargo);
+UserRoutes.post("/registerUtil", upload.single("image"), registerUtil);
+UserRoutes.post("/resend", resendCode);
+UserRoutes.post("/check", checkNewUser);
+
+//!------------------------------------------------------------------------
+//?--------------------------------RUTAS CON REDIRECT----------------------
+//!------------------------------------------------------------------------
+UserRoutes.post("/register", upload.single("image"), registerWithRedirect);
+
+//!---------------- REDIRECT-------------------------------
+UserRoutes.post("/register/sendMail/:id", sendMailRedirect);
+module.exports = UserRoutes;
