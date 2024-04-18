d869799253eddd6c2421c330389af9282074a9e5
pedroleni <pedroleridanieto@hotmail.com>
2024-04-18 14:22:55 +0200

    get

---
+++ MODULO 2/Creacion de API rest/4.-Controladores y rutas/4.8. Delete y lecturas/src/api/routes/User.routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const UserRoutes = express.Router();
+
+const {
+  registerLargo,
+  registerUtil,
+  registerWithRedirect,
+  sendMailRedirect,
+  resendCode,
+  checkNewUser,
+  login,
+  autoLogin,
+  changePassword,
+  sendPassword,
+  modifyPassword,
+  update,
+  deleteUser,
+  getAll,
+  byId,
+  byName,
+  byGender,
+} = require("../controllers/User.controllers");
+const { upload } = require("../../middleware/files.middleware");
+const { isAuth, isAuthAdmin } = require("../../middleware/auth.middleware");
+
+//!------------------------------------------------------------------------
+//?--------------------------------RUTAS SIN REDIRECT----------------------
+//!------------------------------------------------------------------------
+
+UserRoutes.post("/registerLargo", upload.single("image"), registerLargo);
+UserRoutes.post("/registerUtil", upload.single("image"), registerUtil);
+UserRoutes.post("/resend", resendCode);
+UserRoutes.post("/check", checkNewUser);
+UserRoutes.post("/login", login);
+UserRoutes.post("/login/autologin", autoLogin);
+UserRoutes.patch("/forgotpassword", changePassword);
+UserRoutes.patch("/changepassword", [isAuth], modifyPassword);
+UserRoutes.patch("/update/update", [isAuth], upload.single("image"), update);
+UserRoutes.delete("/", [isAuth], deleteUser);
+UserRoutes.get("/", getAll);
+UserRoutes.get("/finById/:id", byId);
+UserRoutes.get("/finByName/:name", byName);
+UserRoutes.get("/finByGender/:gender/:name", byGender);
+
+//!------------------------------------------------------------------------
+//?--------------------------------RUTAS CON REDIRECT----------------------
+//!------------------------------------------------------------------------
+UserRoutes.post("/register", upload.single("image"), registerWithRedirect);
+
+//!---------------- REDIRECT-------------------------------
+UserRoutes.post("/register/sendMail/:id", sendMailRedirect);
+UserRoutes.patch("/sendPassword/:id", sendPassword);
+module.exports = UserRoutes;
